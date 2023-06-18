d084769917eb933ccb77412663200b9d55d6864d
max-marschhauser <max.marschhauser@gmail.com>
2023-06-18 11:37:05 +0200

    bug fixes

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'package:meals/providers/meals_provider.dart';
-import 'package:meals/models/meal.dart';
 import 'package:meals/screens/categories.dart';
 import 'package:meals/screens/filters.dart';
 import 'package:meals/screens/meals.dart';
